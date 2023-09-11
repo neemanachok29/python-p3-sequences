86eae19d3955af81a6a435b1562a036f4ff28b65
Neema Nachok <neemanachok29@gmail.com>
2023-09-11 08:31:56 +0300

    submit

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,13 @@
 
 def print_fibonacci(length):
     pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+        print(fib_sequence)
